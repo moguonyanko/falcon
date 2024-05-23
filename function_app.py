2f575c79eaa899dcd16df58f89e7f4b830b31a79
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2024-05-24 00:21:39 +0900

    Initial commit

---
+++ function_app.py
@@ -0,0 +1,25 @@
+import azure.functions as func
+import logging
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+
+@app.route(route="HelloWorld")
+def HelloWorld(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('HelloWorld request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"こんにちは, {name}。関数アプリ呼び出しは成功しました！")
+    else:
+        return func.HttpResponse(
+             "関数アプリ呼び出し失敗",
+             status_code=400
+        )
