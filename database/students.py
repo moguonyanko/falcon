b17a0d096f8211210a4821c9ebe9c0d49ff0b6cf
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2024-05-30 00:53:57 +0900

    MariaDBのモジュールを参照してみた 関数アプリではModuleNotFoundErrorになることに変化はなかった

--- database/students.py
+++ database/students.py
@@ -1,29 +1,36 @@
 import azure.functions as func
 import logging
-import mysql.connector
+import json
+import mariadb
 
 students = func.Blueprint()
 
-async def get_students():
-    db = mysql.connector.connect(
-        host="localhost",
-        user="sampleuser",
-        password="samplepass",
-        database="test"
-    )
-    cursor = db.cursor()
+def get_students() -> list[any]:
+    try:
+        con = mariadb.connect(
+            host="localhost",
+            user="sampleuser",
+            password="samplepass",
+            database="test"
+        )
+    except mariadb.Error as err:
+        print(f'Connection failed:{err}')
+        return []
+    
+    cursor = con.cursor()
     cursor.execute("SELECT * FROM students")
-    results = cursor.fetchall()
-    return results        
+    students = cursor.fetchall()
+    return students        
 
 @students.route(route="Students", auth_level=func.AuthLevel.FUNCTION)
 def Students(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Students request.')
 
-    results = get_students()
+    students = get_students()
 
-    if len(results) > 0:
-        return func.HttpResponse(students)
+    if len(students) > 0:
+        return func.HttpResponse(json.dumps(students), 
+                                 status_code=200)
     else:
         return func.HttpResponse(
              "Failded get student records",
